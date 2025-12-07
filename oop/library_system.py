278446d4384720602dbb037c3ed5c2fa879bcae7
kojolowlow1 <bryanahwireng10@gmail.com>
2025-12-07 23:14:27 +0000

    Initial commit

---
+++ oop/library_system.py
@@ -0,0 +1,38 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return F"Book: {self.title} by {self.author}"
+        
+
+class EBOOK(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBOOK: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self,book):
+        if isinstance(book,Book):
+            self.books.append(book)
+            print(f"Added {book.title} to library")
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
