85ce1b7b1977af293180486c47f7a284123a5ebc
kojolowlow1 <bryanahwireng10@gmail.com>
2025-12-07 21:07:08 +0000

    add ,missing parenthesis in repr method

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,5 +11,9 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year}"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+
+book1 = Book("The three little Angels", "Bryannomis the Great", 2025)
+print(book1)
         
