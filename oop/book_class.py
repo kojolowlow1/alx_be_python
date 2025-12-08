189114e2e0c777acde073a99fbd2f4cf26519309
kojolowlow1 <bryanahwireng10@gmail.com>
2025-12-08 12:57:04 +0000

    fix print error

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,18 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title, author, year):
+        """Initialize a Book instance."""
         self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        print (f"Deleting {self.title}")
+        """Destructor that prints when a book is deleted."""
+        print(f"Deleting {self.title}")
 
     def __str__(self):
+        """Informal string representation."""
         return f"{self.title} by {self.author}, published in {self.year}"
-    
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
-    
 
-book1 = Book("The three little Angels", "Bryannomis the Great", 2025)
-print(book1)
-        
+    def __repr__(self):
+        """Official representation used for debugging and recreation."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
